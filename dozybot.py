1ac1e735a1c7f2065f55d27bdf9ce4dc4ee0308c
Matt <dozy-morgan@hotmail.co.uk>
2015-12-23 11:51:48 +0000

    Added list of servers joined.

--- dozybot.py
+++ dozybot.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import discord
 from random import randint
 from botconfig import *
@@ -19,10 +20,18 @@ def on_message(message):
 !hello = replies a greeting and your name
 !help = list the commands
 !roll = roll a random number between 0 and 100
+!servers = list the servers this bot is on
 !ts = teamspeak IP address
 !whats up = obvious
         """)
+    #Images
+    if message.content.startswith('!kappa'):
+        client.send_file(message.channel, './images/Kappa.png')
 
+    if message.content.startswith('!bigkappa'):
+        client.send_file(message.channel, './images/Kappahd.png')
+
+    #Random rolls
     if message.content.startswith('!coin'):
         roll = randint(0, 1)
         if (roll == 0):
@@ -31,19 +40,24 @@ def on_message(message):
             coin = "tails"
         client.send_message(message.channel, '{} flipped a coin and it landed on '.format(message.author.mention()) + coin)
 
-    if message.content.startswith('!kappa'):
-        client.send_file(message.channel, './images/Kappa.png')
-
-    if message.content.startswith('!bigkappa'):
-        client.send_file(message.channel, './images/Kappahd.png')
-
     if message.content.startswith('!roll'):
         roll = randint(0, 100)
         client.send_message(message.channel, '{} rolled a '.format(message.author.mention()) + str(roll))
 
+    # Utility
     if message.content.startswith('!hello'):
         client.send_message(message.channel, 'Hello {}!'.format(message.author.mention()))
 
+    if message.content.startswith('!servers'):
+        servers = []
+        for name in client.servers:
+            servers.insert(0 , name.name)
+        client.send_message(message.channel, ', '.join(servers))
+
+    if message.content.startswith('!ts'):
+        client.send_message(message.channel, 'Teamspeak IP: 88.202.226.151:7297')
+
+    # Fun stuff
     if message.content.startswith('!liu'):
         liu = discord.utils.find(lambda m: m.id == '105714564930260992', message.channel.server.members)
         if (liu == None):
@@ -54,13 +68,17 @@ def on_message(message):
     if message.content.startswith('!whats up'):
         client.send_message(message.channel, 'The sky.')
 
-    if message.content.startswith('!ts'):
-        client.send_message(message.channel, 'Teamspeak IP: 88.202.226.151:7297')
 
 @client.event
 def on_ready():
     print('Connected!')
     print('Username: ' + client.user.name)
     print('ID: ' + client.user.id)
+    # server = discord.utils.find(lambda s: s.name == 'Dozy Bot Testing Grounds', client.servers)
+    # print(server.id)
+    servers = []
+    for name in client.servers:
+        servers.insert(0 , name.name)
+    print (servers[0])
 
 client.run()
